82d64bd013d069f35446db4c38af273f3cf1e692
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-02-04 18:25:29 -0300

    Finzaliando user list

--- src/app/components/user/user-list/user-list.component.ts
+++ src/app/components/user/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -10,28 +11,34 @@ import { User } from 'src/app/models/user';
 })
 export class UserListComponent implements OnInit {
 
-  ELEMENT_DATA: User[] = [
-    {
-      id: 1,
-      name: 'Marcos Andrade',
-      email: 'marcos@email.com',
-      password: '123',
-      roles: ['0']
-    }
-  ]
+  ELEMENT_DATA: User[] = []
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','perfil','acoes'];
+  displayedColumns: string[] = ['id', 'nome', 'email','perfil','acoes'];
   dataSource = new MatTableDataSource<User>(this.ELEMENT_DATA);
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  constructor(private service: UserService) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta
+      this.dataSource = new MatTableDataSource<User>(resposta);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
   }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${API_CONFIG.baseUrl}/users`);
+  }
+}
